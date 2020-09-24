cdd2e7b7ccbd2858d7b21ccabf4d9926247e9f55
LezMalmo <70897432+LezMalmo@users.noreply.github.com>
2020-09-24 16:49:32 +0200

    updated

--- Labb/src/com/company/Patient.java
+++ Labb/src/com/company/Patient.java
@@ -3,7 +3,7 @@ package com.company;
 public class Patient {
     private String name;
     private String sickness;
-    boolean isSick = false;
+    private boolean isSick = false;
 
     //Constructor 1
     public Patient (String name, String sickness) {
@@ -16,26 +16,22 @@ public class Patient {
     }
 
     //Method 1
-    public String getName() {
+    protected String getName() {
         return name;
     }
     //Method 2
-    public String getSickness(){
+    protected String getSickness(){
         return sickness;
     }
     //Method 3
-    public void takeMedication(Medicine medicine){
+    protected void takeMedication(Medicine medicine){
         if (medicine.getTreatmentName().equals(this.sickness)) {
             this.sickness = null;
         }
     }
     //Method 4
-    public boolean isSick() {
-        if (sickness == null) {
-            isSick = false;}
-        else {
-            isSick = true;
-        }
+    protected boolean isSick() {
+        isSick = sickness != null;
         return isSick;
     }
 }
